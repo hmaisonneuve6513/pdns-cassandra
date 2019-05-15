f80479d156060510f0180ea2db40dd573a87e1f4
herve <hmaisonneuve6513@gmail.com>
2019-05-15 22:55:22 +0200

    initialise replacerrset function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -163,6 +163,8 @@ def replace_rrset(id,qname,qtype):
         'INSERT INTO records (domain_id, qname, content, disabled, qtype, ttl ) VALUES ( %s, %s, %s, 0, %s, 3600)', (domain_id,qname,content,qtype,)
     )
 
+    return 'true'
+
 @app.route('/superMasterBackend/<ip>/<domain>', methods=['POST'])
 def super_master_backend(ip, domain):
     ''' check if we can be a slave for a domain '''
