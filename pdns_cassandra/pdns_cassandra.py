ccf19c1a32dccbb3954f40c07f1b067b34ecac49
herve <hmaisonneuve6513@gmail.com>
2019-05-15 23:01:20 +0200

    initialise replacerrset function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -163,9 +163,10 @@ def replace_rrset(id,qname,qtype):
         'INSERT INTO records (domain_id, qname, content, disabled, qtype, ttl ) VALUES ( %s, %s, %s, 0, %s, 3600)', (domain_id,qname,content,qtype,)
     )
 
-    print result[0]
-
-    return 'true'
+    if result:
+        return 'true'
+    else:
+        return 'false'
 
 @app.route('/superMasterBackend/<ip>/<domain>', methods=['POST'])
 def super_master_backend(ip, domain):
