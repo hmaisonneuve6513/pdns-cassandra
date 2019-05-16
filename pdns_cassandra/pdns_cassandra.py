bd86d9df73b1a1bfbb39c2afe22e322c7f207c05
herve <hmaisonneuve6513@gmail.com>
2019-05-16 14:37:54 +0200

    initialise searchRecords function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -207,7 +207,7 @@ def searchRecords():
             rrset = get_or_404(
                 'SELECT domain_id, qname, content, disabled, qtype, ttl FROM records WHERE domain_id = %s LIMIT %s ALLOW FILTERING', (param_qname,param_max,)
             )
-        elif rcname_len >=2:
+        elif rcname_len >3:
             rrset = get_or_404(
                 'SELECT domain_id, qname, content, disabled, qtype, ttl FROM records WHERE qname = %s LIMIT %s ALLOW FILTERING', (param_qname,param_max,)
             )
