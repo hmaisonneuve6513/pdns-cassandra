730707017dbca9d99ded7bc457f51d63b88815db
herve <hmaisonneuve6513@gmail.com>
2019-05-16 11:18:34 +0200

    initialise searchRecords function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -206,7 +206,6 @@ def searchRecords():
     rrset = get_or_404(
         'SELECT domain_id, qname, content, disabled, qtype, ttl FROM records WHERE  qname = %s LIMIT %s ALLOW FILTERING', (param_qname,param_max,)
     )
-    '''
     
     for record in rrset:
         inter = dict (
@@ -218,10 +217,6 @@ def searchRecords():
             ttl = record['ttl'],
         )
         result.append(inter)
-    '''
-
-    for record in rrset:
-        result.append(record)
 
     return jsonify(result=result)
 
