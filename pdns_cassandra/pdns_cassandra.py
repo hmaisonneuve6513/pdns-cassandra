eff1e3c9a89ac0f11536ed69786455d0ff06752e
herve <hmaisonneuve6513@gmail.com>
2019-06-10 17:25:31 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -204,7 +204,7 @@ def replace_rrset(p_id,p_qname,p_qtype):
 
         print 'select item to destroy:'
         rows = get_or_404(
-            'SELECT * FROM records WHERE  qname = %s and content = %s LIMIT 1', (rrset['qname'], '192.168.123.99', )
+            'SELECT * FROM records WHERE  qname = %s and content = %s ALLOW FILTERING', (rrset['qname'], '192.168.123.99', )
         )
         if rows:
             count = 0
