0852c30077fc69cd8b57aa55495c8de6c281093c
herve <hmaisonneuve6513@gmail.com>
2019-06-10 13:25:12 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -164,7 +164,8 @@ def replace_rrset(id,qname,qtype):
     print id
     print qtype
     print 'Parameter recuperation'
-    rrsets = request.args.getlist('rrsets')
+    rrsets = request.args.get('rrsets')
+    print request.query_params
 
     for rrset in rrsets:
 
