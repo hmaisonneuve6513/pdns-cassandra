d5da55324bb5a304436f4e1a78cd9b6d62bf256c
herve <hmaisonneuve6513@gmail.com>
2019-06-10 15:51:42 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -44,6 +44,9 @@ def suppresses_form_header(stringtocut):
     result = stringtocut[2:]
     return result
 
+def parse_to_rrset(stringtoparse)
+    return stringtoparse
+
 @app.route('/lookup/<qname>/<qtype>')
 def lookup(qname, qtype):
     ''' do a basic query '''
