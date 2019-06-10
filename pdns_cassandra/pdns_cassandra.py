db1d28e0e495fba4d9d12bb2e01b2f5fbc60fcb2
herve <hmaisonneuve6513@gmail.com>
2019-06-10 16:20:03 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -231,8 +231,8 @@ def replace_rrset(id,qname,qtype):
             print "New content: " + rrset['content']
 
             insert = get_or_404(
-                'INSERT INTO records (domain_id, qname, content, auth, disabled, ordername, qtype, ttl ) VALUES ( %s, %s, %s, 0, %s, %s, %s, %s, %s, %s )',
-                (r['domain_id'], r['qname'], rrset['content'], 0, 0, '', r['qtype'], 3600)
+                'INSERT INTO records (domain_id, qname, content, auth, disabled, qtype, ttl ) VALUES ( %s, %s, %s, 0, %s, %s, %s, %s, %s, %s )',
+                (r['domain_id'], r['qname'], rrset['content'], 0, 0, r['qtype'], 3600)
             )
             count += count
             print count
