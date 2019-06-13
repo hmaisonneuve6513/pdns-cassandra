4b24ce06ab772b52ce74848eae393329fa729093
herve <hmaisonneuve6513@gmail.com>
2019-06-13 14:17:05 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -69,9 +69,9 @@ def parse_to_rrset(stringtoparse):
     rrset_inter = []
     rrsets = []
 
-    '''additional = '&2&rrset[1][content]=192.168.123.112&rrset[1][qclass]=1&rrset[1][qname]=ftp.osnworld.net.&rrset[1][qtype]=A&rrset[1][ttl]=3600'
+    '''additional = '&2&rrset[1][content]=192.168.123.112&rrset[1][qclass]=1&rrset[1][qname]=ftp.osnworld.net.&rrset[1][qtype]=A&rrset[1][ttl]=3600' '''
 
-    parameters = stringtoparse+additional'''
+    parameters = stringtoparse
     parameters = parameters.split("&")
 
     for out_rrsets in parameters:
