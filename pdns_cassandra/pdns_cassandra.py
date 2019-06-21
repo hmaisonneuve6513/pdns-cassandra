777a54d30e5d1bc17574990f472bf0456bc1dde4
herve <hmaisonneuve6513@gmail.com>
2019-06-21 10:51:10 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -338,7 +338,7 @@ def set_domain_metadata(domain_id, kind):
     inter_str = inter_array[0]
 
     val_array = inter_str.split('=')
-    val = val_array[1])
+    val = val_array[1]
 
     print 'Check Item presence'
     check = command( 'SELECT domain_id FROM domain_metadata WHERE domain_id = %s and kind = %s', ( domain_id, kind, ) )
