358cd207a0790e42187e8edb3ffcb6798bbd7336
herve <hmaisonneuve6513@gmail.com>
2019-06-21 16:07:59 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -299,7 +299,7 @@ def get_all_domain_metadata(domain_id):
         value = meta['content']
         print 'Value : '+ value
         #inter[prop] = value
-        inter = prop+'='+value
+        inter = '&'+prop+'='+value
         print inter
         result.append(inter)
         print result
