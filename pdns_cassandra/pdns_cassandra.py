04f0240930afdaf7f1f7d451cf8dc2177bb71b67
herve <hmaisonneuve6513@gmail.com>
2019-06-21 15:20:08 +0200

    initialise setnotified function

--- pdns_cassandra/pdns_cassandra.py
+++ pdns_cassandra/pdns_cassandra.py
@@ -295,10 +295,14 @@ def get_all_domain_metadata(domain_id):
     metadatas = get_or_404('SELECT kind, content FROM domain_metadata WHERE domain_id = %s ALLOW FILTERING', (domain_id, ) )
     for meta in metadatas:
         prop = meta['kind']
+        print 'Prop : '+pop
         value = meta['content']
+        print 'Value : '+ value
         inter[prop] = value
 
+        print inter
         result.append(inter)
+        print result
 
     return jsonify(result=result)
 
